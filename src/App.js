17623746cfc347b4c52c0077f9776343634a4770
= <55615890+Kevin-Romero@users.noreply.github.com>
2023-03-09 22:57:23 -0800

    initialization

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App w-screen h-screen bg-cyan-200">
+      <div>Hola Mundo</div>
+    </div>
+  );
+}
+
+export default App;
