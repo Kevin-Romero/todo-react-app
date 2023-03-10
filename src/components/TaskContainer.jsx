0d433d6abd8a21410d4a2cd1c191939844fe1a92
= <55615890+Kevin-Romero@users.noreply.github.com>
2023-03-10 00:17:07 -0800

    Completed TaskForm component style

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -3,7 +3,7 @@ import TaskForm from "./TaskForm";
 
 function TaskContainer() {
   return (
-    <div className="w-11/12 max-w-xl h-auto bg-cyan-400 drop-shadow-xl	">
+    <div className="w-11/12 max-w-xl h-auto bg-cyan-400 drop-shadow-xl px-3 py-2 mt-3">
       <TaskForm />
     </div>
   );
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -6,10 +6,12 @@ function TaskForm() {
       <input
         type="text"
         name="taskInput"
-        className=""
+        className="px-3 py-2 outline-none text-cyan-700 w-4/5"
         placeholder="What is your task?"
       />
-      <button type="submit">Add</button>
+      <button type="submit" className="bg-cyan-700 text-white px-3 py-2 w-1/5">
+        Add
+      </button>
     </form>
   );
 }
