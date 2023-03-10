3344bb4dc5a2715829a7f892cc9dc9b33ed1e61a
= <55615890+Kevin-Romero@users.noreply.github.com>
2023-03-09 23:08:34 -0800

    create title project

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App w-screen h-screen bg-cyan-200">
-      <div>Hola Mundo</div>
+    <div className="App">
+      <div className="w-screen h-screen bg-cyan-200 flex justify-center items-center flex-wrap uppercase text-center">
+        <h1 className="font-bold text-5xl text-cyan-700">todo react app</h1>
+      </div>
     </div>
   );
 }
